552606e94b0530273d448b6e65c91f7dd4d4210d
Imron jehleh <imron.jehleh@gmail.com>
2024-02-27 01:03:19 +0700

    20240227 T0103 CPE2312: VStudio really needs me to initialize all data. What an headache.

--- #2/automata/internal/test.c
+++ #2/automata/internal/test.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63bd1d60c6ce60e26845dfaa41ddd7b93a398164e4cea6eac54110520689d4d6
-size 4120
+oid sha256:0a05ebd8325a0f4ced1c23914934a408b185e04c88f36f02ff9e8bff8964760c
+size 4124
