2969f195776e317508b2efc551ec4aec56e421f2
Imron jehleh <imron.jehleh@gmail.com>
2024-02-26 18:19:59 +0700

    20240226 T1819 CPE2312: I see... failContainer didn't generate correctly as expected.

--- #2/automata/internal/setup.c
+++ #2/automata/internal/setup.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:474fed95dfcf087fa2fbc4f77b4c6431ad09362a9068b88a6bb4ae1387c0aaf3
-size 884
+oid sha256:53d4e6a550abaf6b143058f47461d592879deebdcfe8da67ef8b35dc9a259aa1
+size 1076
--- #2/automata/internal/strGen.c
+++ #2/automata/internal/strGen.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06262e200ed64a1cd13a901c1659cff7d589002aa436b34e498f6221c0b05459
-size 2848
+oid sha256:15a58e2e56333e60bf7ffdf498e582ec575eae9fa94b9dc5901984e8845e6979
+size 2889
