75b20211dac899a2e8f998fc07d2016e1709c41a
Imron jehleh <imron.jehleh@gmail.com>
2024-02-26 17:54:48 +0700

    20240226 T1754 CPE2312: Try to debug where it is going wrong.

--- #2/automata/internal/automata.h
+++ #2/automata/internal/automata.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d88a4fc0d9b73a25c2e2c5fd59762dda347d05e9d4633cc7ed1a81428e7b796
-size 598
+oid sha256:9335e1154c4f3c6e836b855a230501731df7e4041a6d71c7c26d10dc8f2daeeb
+size 634
