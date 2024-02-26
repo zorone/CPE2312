2c1720c0f9a5891533eb40f1a221cbb71f1907c6
Imron jehleh <imron.jehleh@gmail.com>
2024-02-26 17:14:28 +0700

    20240226 T1707 CPE2312: Such stupid mistake, I needs to called the program first so it could run. But I wrote the codes to prevent myself from calling the program.

--- #2/internal/test.c
+++ #2/internal/test.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e4e44f22304190c8c97bfaafa50764fdc06c4c0b5adbb51c180124fc9d18a60
-size 4039
+oid sha256:9f168e0108dee6a33a5b56a9e829e1e5fb92eceaeaa72aea98f9091d441ba2f2
+size 4036
--- #2/main.c
+++ #2/main.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:940245cdbf39d937355aa02a9b42da0966dbf79e66cfed3d43654147a9ce2163
-size 570
+oid sha256:f004057297834cde1709f7f672765eb8ff616ea5c9e095c1a0ee43f9a6ae3337
+size 429
