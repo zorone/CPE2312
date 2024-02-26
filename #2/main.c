9c880f7b13e312ef5f8756153e0c6ee3f620b057
Imron jehleh <imron.jehleh@gmail.com>
2024-02-26 17:08:13 +0700

    20240226 T1707 CPE2312: Yes!!! Now it works.
    Also, forgot to change allocateSize.

--- #2/main.c
+++ #2/main.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e96ca28a7e564452bef749f06726df74241e09cc72e2960283ff330ed1b64d6
-size 577
+oid sha256:940245cdbf39d937355aa02a9b42da0966dbf79e66cfed3d43654147a9ce2163
+size 570
