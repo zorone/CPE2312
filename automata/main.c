14d84db651037fb0664e8ee942025b0ff60aae1c
Imron jehleh <imron.jehleh@gmail.com>
2024-03-03 15:47:41 +0700

    20240303 T1547 CPE2312: Add more todo.

--- automata/main.c
+++ automata/main.c
@@ -3,6 +3,14 @@
 // TODO: Seperate Debug codes out completely. If possible, function that you would like to inspect, should pass into debug() function.
 // TODO: Add user interaction mode -i, that it waits for user testing string.
 
+// TODO: Add support for Quiet -q
+// TODO: Add support for Progress bar -P
+// TODO: Add support for no cached -n
+// TODO: Add support for only report -R
+
+// TODO: Add support for no recursive (for NFA -- check only until it is accept.) -r
+// TODO: Add support for Listing all route (for NFA) -l
+
 int main(int argc, char *argv[]){
     init();
     config();
